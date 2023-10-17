26ad1efc9de7a5bdfbd1a26e87ae7dbf339bcb94
xinxing.wang <xinxing.wang@megatronix.co>
2023-10-17 13:14:47 +0800

    Update
    add the xLIST type firstly to file.

---
+++ portable/template/portmacro.h
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------
+ * Port specific definitions.
+ *
+ * The settings in this file configure Novax corretyly for the 
+ * given hardware and compiler.
+ * 
+ * These settings should not be altered.
+ * ----------------------------------------------------------
+ */
+
+/* Type definitions. */
